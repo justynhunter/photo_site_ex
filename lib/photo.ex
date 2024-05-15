53cd805307558bb9c239a12da7d3949387e76da4
Justyn Hunter <justynhunter@gmail.com>
2024-05-15 11:40:29 -0500

    adds migrations

---
+++ lib/photo.ex
@@ -0,0 +1,11 @@
+defmodule PhotoSite.Photo do
+  use Ecto.Schema
+
+  schema "photo" do
+    field :seq, :integer
+    field :src, :string
+    field :alt, :string
+    field :created_at, :utc_datetime
+    belongs_to :album, PhotoSite.Album
+  end
+end
