e36f082e6448d92c34ca570b68731a0655ec75c5
Justyn Hunter <justynhunter@gmail.com>
2024-05-17 09:57:50 -0500

    adds hooks

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,14 +21,13 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Hooks from "./hooks"
 import { TurnstileHook } from "phoenix_turnstile"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
     params: {_csrf_token: csrfToken},
-    hooks: {
-        Turnstile: TurnstileHook
-    }
+    hooks: {...Hooks, TurnstileHook}
 })
 
 // Show progress bar on live navigation and form submits
