41834668a39a572c15a5cf13eec116ceccc859d2
Justyn Hunter <jhunter@gsandf.com>
2024-05-17 12:47:04 -0500

    reorder photos in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -19,9 +19,9 @@ unless Repo.exists?(Album, name: "default") do
 
   {_, default_album} = Repo.insert(default_album)
 
-  photo1 = %Photo{album_id: default_album.id, seq: 1, src: "https://live.staticflickr.com/65535/53684851241_bac2ceb36b_b.jpg", alt: "rotting house"}
+  photo1 = %Photo{album_id: default_album.id, seq: 1, src: "https://live.staticflickr.com/65535/53609244371_184f1a88c1_b.jpg", alt: "rotting house"}
   photo2 = %Photo{album_id: default_album.id, seq: 2, src: "https://live.staticflickr.com/65535/53684852131_5d1a0c2a54_b.jpg", alt: "chimney"}
-  photo3 = %Photo{album_id: default_album.id, seq: 3, src: "https://live.staticflickr.com/65535/53609244371_184f1a88c1_b.jpg", alt: "rotting house"}
+  photo3 = %Photo{album_id: default_album.id, seq: 3, src: "https://live.staticflickr.com/65535/53684851241_bac2ceb36b_b.jpg", alt: "rotting house"}
 
   Repo.insert(photo1)
   Repo.insert(photo2)
