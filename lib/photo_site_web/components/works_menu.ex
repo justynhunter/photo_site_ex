d8600372783dd028c7145f637f242e67c812a6d7
Justyn Hunter <jhunter@gsandf.com>
2024-05-21 09:38:36 -0500

    remove javascript

--- lib/photo_site_web/components/works_menu.ex
+++ lib/photo_site_web/components/works_menu.ex
@@ -6,25 +6,30 @@ defmodule WorksMenuComponent do
 
   def mount(socket) do
     albums = get_albums()
-    {:ok, assign(socket, albums: albums)}
+    {:ok, assign(socket, albums: albums, show_menu: false)}
   end
 
   def render(assigns) do
     ~H"""
-    <div>
-      <script>
-        function worksClick() {
-            document.getElementById("dropdown").classList.add("show");
-        }
-      </script>
-      <a onclick="worksClick()" id="menu_link">work</a>
-      <ul id="dropdown">
+    <div id="works_dropdown">
+      <a id="menu_link" phx-click="works_click" phx-target={@myself}>work</a>
+      <ul id="dropdown" class={@show_menu}>
         <%= for album <- @albums do %>
           <li><a href={"/album/#{album.id}"}><%= album.name %></a></li>
         <% end %>
       </ul>
     </div>
     """
+  end
+
+  def handle_event("works_click", _params, socket) do
+    socket =
+      case socket.assigns.show_menu == "show" do
+        true -> assign(socket, show_menu: "")
+        false -> assign(socket, show_menu: "show")
+      end
+
+    {:noreply, socket}
   end
 
   def get_albums() do
