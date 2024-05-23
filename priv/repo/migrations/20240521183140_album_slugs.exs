4e3bc4e6843f8bf1d8bfcd3d9d5872733bf6ffab
Justyn Hunter <justynhunter@gmail.com>
2024-05-22 22:16:29 -0500

    squash migrations, seed with correct data

--- priv/repo/migrations/20240521183140_album_slugs.exs
+++
@@ -1,9 +0,0 @@
-defmodule PhotoSite.Repo.Migrations.AlbumSlugs do
-  use Ecto.Migration
-
-  def change do
-    alter table(:album) do
-      add :slug, :string
-    end
-  end
-end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -15,7 +15,7 @@ alias PhotoSite.Album
 alias PhotoSite.Photo
 
 unless Repo.exists?(Album, name: "default") do
-  default_album = %Album{name: "default", description: "default album for home page"}
+  default_album = %Album{name: "selected places", slug: "selected-places", seq: 1, description: "selected places"}
 
   {_, default_album} = Repo.insert(default_album)
 
@@ -26,6 +26,17 @@ unless Repo.exists?(Album, name: "default") do
   Repo.insert(photo1)
   Repo.insert(photo2)
   Repo.insert(photo3)
+
+  wildlife_album = %Album{name: "wildlife", slug: "wildlife", seq: 2, description: "kentucky wildlife museum"}
+  {_, wildlife_album} = Repo.insert(wildlife_album)
+
+  wphoto1 = %Photo{album_id: wildlife_album.id, seq: 1, src: "https://live.staticflickr.com/65535/52953746019_6f8e401686_b.jpg", alt: "ram"}
+  wphoto2 = %Photo{album_id: wildlife_album.id, seq: 2, src: "https://live.staticflickr.com/65535/52954052848_6904cd0887_b.jpg", alt: "birds"}
+  wphoto3 = %Photo{album_id: wildlife_album.id, seq: 3, src: "https://live.staticflickr.com/65535/52953740594_d61afc02e9_b.jpg", alt: "buffalo"}
+
+  Repo.insert(wphoto1)
+  Repo.insert(wphoto2)
+  Repo.insert(wphoto3)
 else
   IO.puts("nothing to seed")
 end
