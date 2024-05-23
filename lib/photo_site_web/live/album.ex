ebb1e0a68cfe8264b12b57cff690b8ba722b6217
Justyn Hunter <10092578+justynhunter@users.noreply.github.com>
2024-05-22 22:18:15 -0500

    Adds works menu to header and albums (#5)
    * remove links to socials
    
    * put socials links on about page
    
    * remove id
    
    * stub out links
    
    * add click event listener
    
    * add some styles
    
    * add album page
    
    * fix javascript
    
    * fix albums
    
    * load album on index
    
    * make works menu component
    
    * format menu
    
    * remove javascript
    
    * clean up a bit
    
    * componentize image carousel
    
    * drop spec
    
    * add slug to album
    
    * use slug from route and passthrough pages
    
    * get album at page and pass to component
    
    * use phoenix link in works menu
    
    * throw up error when not found so 404 page shows
    
    * just go to /slug instead of /album/slug
    
    * external links open in new tab
    
    * animate menu
    
    ---------
    
    Co-authored-by: Justyn Hunter <justynhunter@gmail.com>

---
+++ lib/photo_site_web/live/album.ex
@@ -0,0 +1,22 @@
+defmodule PhotoSiteWeb.AlbumLive do
+  use PhotoSiteWeb, :live_view
+
+  alias PhotoSite.Album
+  alias PhotoSite.Repo
+
+  def mount(params, _session, socket) do
+    album = get_album(params["slug"])
+    {:ok, assign(socket, page_title: album.name, album: album)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <.live_component module={ImageCarousel} id="images" album={@album} />
+    """
+  end
+
+  def get_album(slug) do
+    Repo.get_by!(Album, slug: slug)
+    |> Repo.preload(:photo)
+  end
+end
