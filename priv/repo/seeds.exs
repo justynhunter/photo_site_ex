9b59209fb3fe449d7988b0979385233d88d94268
Justyn Hunter <justynhunter@gmail.com>
2024-06-01 17:13:15 -0500

    adds captions to other photos in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -55,22 +55,22 @@ unless Repo.exists?(Album) do
   default_album = %Album{name: "wildlife", slug: "wildlife", seq: 2, description: "kentucky wildlife museum"}
   {_, default_album} = Repo.insert(default_album)
 
-  photo1 = %Photo{album_id: default_album.id, seq: 1, src: "https://photos.justynhunter.com/wildlife-1.webp", alt: "ram"}
-  photo2 = %Photo{album_id: default_album.id, seq: 2, src: "https://photos.justynhunter.com/wildlife-2.webp", alt: "butterflys"}
-  photo3 = %Photo{album_id: default_album.id, seq: 3, src: "https://photos.justynhunter.com/wildlife-3.webp", alt: "penguins"}
-  photo4 = %Photo{album_id: default_album.id, seq: 4, src: "https://photos.justynhunter.com/wildlife-4.webp", alt: "weasel"}
-  photo5 = %Photo{album_id: default_album.id, seq: 5, src: "https://photos.justynhunter.com/wildlife-5.webp", alt: "insects"}
-  photo6 = %Photo{album_id: default_album.id, seq: 6, src: "https://photos.justynhunter.com/wildlife-6.webp", alt: "buffalo"}
-  photo7 = %Photo{album_id: default_album.id, seq: 7, src: "https://photos.justynhunter.com/wildlife-7.webp", alt: "birds"}
-  photo8 = %Photo{album_id: default_album.id, seq: 8, src: "https://photos.justynhunter.com/wildlife-8.webp", alt: "bears"}
-  photo9 = %Photo{album_id: default_album.id, seq: 9, src: "https://photos.justynhunter.com/wildlife-9.webp", alt: "lion"}
-  photo10 = %Photo{album_id: default_album.id, seq: 10, src: "https://photos.justynhunter.com/wildlife-10.webp", alt: "badger"}
-  photo11 = %Photo{album_id: default_album.id, seq: 11, src: "https://photos.justynhunter.com/wildlife-11.webp", alt: "snakes"}
-  photo12 = %Photo{album_id: default_album.id, seq: 12, src: "https://photos.justynhunter.com/wildlife-12.webp", alt: "snake"}
-  photo13 = %Photo{album_id: default_album.id, seq: 13, src: "https://photos.justynhunter.com/wildlife-13.webp", alt: "butterflys"}
-  photo14 = %Photo{album_id: default_album.id, seq: 14, src: "https://photos.justynhunter.com/wildlife-14.webp", alt: "beaver"}
-  photo15 = %Photo{album_id: default_album.id, seq: 15, src: "https://photos.justynhunter.com/wildlife-15.webp", alt: "peacock"}
-  photo16 = %Photo{album_id: default_album.id, seq: 16, src: "https://photos.justynhunter.com/wildlife-16.webp", alt: "antelope"}
+  photo1 = %Photo{album_id: default_album.id, seq: 1, src: "https://photos.justynhunter.com/wildlife-1.webp", alt: "ram", caption: "Bighorn Sheep"}
+  photo2 = %Photo{album_id: default_album.id, seq: 2, src: "https://photos.justynhunter.com/wildlife-2.webp", alt: "butterflys", caption: "Butterflies" }
+  photo3 = %Photo{album_id: default_album.id, seq: 3, src: "https://photos.justynhunter.com/wildlife-3.webp", alt: "penguins", caption: "Penguins" }
+  photo4 = %Photo{album_id: default_album.id, seq: 4, src: "https://photos.justynhunter.com/wildlife-4.webp", alt: "otter and wolverine", caption: "Otter and wolverine" }
+  photo5 = %Photo{album_id: default_album.id, seq: 5, src: "https://photos.justynhunter.com/wildlife-5.webp", alt: "insects", caption: "Insect Case" }
+  photo6 = %Photo{album_id: default_album.id, seq: 6, src: "https://photos.justynhunter.com/wildlife-6.webp", alt: "buffalo", caption: "Bison" }
+  photo7 = %Photo{album_id: default_album.id, seq: 7, src: "https://photos.justynhunter.com/wildlife-7.webp", alt: "birds", caption: "Birds" }
+  photo8 = %Photo{album_id: default_album.id, seq: 8, src: "https://photos.justynhunter.com/wildlife-8.webp", alt: "bears", caption: "Black Bears" }
+  photo9 = %Photo{album_id: default_album.id, seq: 9, src: "https://photos.justynhunter.com/wildlife-9.webp", alt: "lion", caption: "Ocelot and Lion" }
+  photo10 = %Photo{album_id: default_album.id, seq: 10, src: "https://photos.justynhunter.com/wildlife-10.webp", alt: "badger", caption: "Badger" }
+  photo11 = %Photo{album_id: default_album.id, seq: 11, src: "https://photos.justynhunter.com/wildlife-11.webp", alt: "pheasants", caption: "Pheasants" }
+  photo12 = %Photo{album_id: default_album.id, seq: 12, src: "https://photos.justynhunter.com/wildlife-12.webp", alt: "snake", caption: "Snake" }
+  photo13 = %Photo{album_id: default_album.id, seq: 13, src: "https://photos.justynhunter.com/wildlife-13.webp", alt: "butterflys", caption: "Butterfly Case" }
+  photo14 = %Photo{album_id: default_album.id, seq: 14, src: "https://photos.justynhunter.com/wildlife-14.webp", alt: "possum", caption: "Possum" }
+  photo15 = %Photo{album_id: default_album.id, seq: 15, src: "https://photos.justynhunter.com/wildlife-15.webp", alt: "peacock", caption: "Peacock" }
+  photo16 = %Photo{album_id: default_album.id, seq: 16, src: "https://photos.justynhunter.com/wildlife-16.webp", alt: "mule deer", caption: "Mule Deer"}
 
   Repo.insert(photo1)
   Repo.insert(photo2)
@@ -98,19 +98,19 @@ unless Repo.exists?(Album) do
 
   {_, default_album} = Repo.insert(default_album)
 
-  photo1 = %Photo{album_id: default_album.id, seq: 1, src: "https://photos.justynhunter.com/church-1.webp", alt: "church"}
-  photo2 = %Photo{album_id: default_album.id, seq: 2, src: "https://photos.justynhunter.com/church-2.webp", alt: "church"}
-  photo3 = %Photo{album_id: default_album.id, seq: 3, src: "https://photos.justynhunter.com/church-3.webp", alt: "church"}
-  photo4 = %Photo{album_id: default_album.id, seq: 4, src: "https://photos.justynhunter.com/church-4.webp", alt: "church"}
-  photo5 = %Photo{album_id: default_album.id, seq: 5, src: "https://photos.justynhunter.com/church-5.webp", alt: "church"}
+  photo1 = %Photo{album_id: default_album.id, seq: 1, src: "https://photos.justynhunter.com/church-1.webp", alt: "church", caption: "Clayborne Chapel AME - Williamsport, Tennessee - 2022"}
+  photo2 = %Photo{album_id: default_album.id, seq: 2, src: "https://photos.justynhunter.com/church-2.webp", alt: "church", caption: "Church - Guthrie, Kentucky - 2022"}
+  photo3 = %Photo{album_id: default_album.id, seq: 3, src: "https://photos.justynhunter.com/church-3.webp", alt: "church", caption: "King's Chapel - Murfreesboro, Tennessee - 2021"}
+  photo4 = %Photo{album_id: default_album.id, seq: 4, src: "https://photos.justynhunter.com/church-4.webp", alt: "church", caption: "Leipers Fork Primitive Baptist Church - Liepers Fork, Tennessee - 2021"}
+  photo5 = %Photo{album_id: default_album.id, seq: 5, src: "https://photos.justynhunter.com/church-5.webp", alt: "church", caption: "7th Street Church - Bell Buckle, Tennessee - 2021"}
   photo6 = %Photo{album_id: default_album.id, seq: 6, src: "https://photos.justynhunter.com/church-6.webp", alt: "church"}
   photo7 = %Photo{album_id: default_album.id, seq: 7, src: "https://photos.justynhunter.com/church-7.webp", alt: "church"}
-  photo8 = %Photo{album_id: default_album.id, seq: 8, src: "https://photos.justynhunter.com/church-8.webp", alt: "church"}
-  photo9 = %Photo{album_id: default_album.id, seq: 9, src: "https://photos.justynhunter.com/church-9.webp", alt: "church"}
-  photo10 = %Photo{album_id: default_album.id, seq: 10, src: "https://photos.justynhunter.com/church-10.webp", alt: "church"}
+  photo8 = %Photo{album_id: default_album.id, seq: 8, src: "https://photos.justynhunter.com/church-8.webp", alt: "church", caption: "New Bethel AME Chuch - Murfreesboro, Tennessee - 2021"}
+  photo9 = %Photo{album_id: default_album.id, seq: 9, src: "https://photos.justynhunter.com/church-9.webp", alt: "church", caption: "Church of Christ at Leipers Form - Leipers Fork, Tennessee - 2021"}
+  photo10 = %Photo{album_id: default_album.id, seq: 10, src: "https://photos.justynhunter.com/church-10.webp", alt: "church", caption: "Little Hurricane Primitive Baptist Church - Manchester, Tennessee - 2021"}
   photo11 = %Photo{album_id: default_album.id, seq: 11, src: "https://photos.justynhunter.com/church-11.webp", alt: "church"}
-  photo12 = %Photo{album_id: default_album.id, seq: 12, src: "https://photos.justynhunter.com/church-12.webp", alt: "church"}
-  photo13 = %Photo{album_id: default_album.id, seq: 13, src: "https://photos.justynhunter.com/church-13.webp", alt: "church"}
+  photo12 = %Photo{album_id: default_album.id, seq: 12, src: "https://photos.justynhunter.com/church-12.webp", alt: "church", caption: "Eureka Curch of Christ - Warren County, Tennessee - 2021"}
+  photo13 = %Photo{album_id: default_album.id, seq: 13, src: "https://photos.justynhunter.com/church-13.webp", alt: "church", caption: "Christian Pentacosal Church - Nashville, Tennessee - 2021"}
 
   Repo.insert(photo1)
   Repo.insert(photo2)
